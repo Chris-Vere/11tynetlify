c7c21d4011756ddc802134b9c9d2b9c3de201fdf
Chris <chris.vere@me.com>
2022-02-11 15:03:20 -0500

    Adds microdata and decouples contact info

--- src/_data/imageData.js
+++ src/_data/imageData.js
@@ -1,6 +1,7 @@
 const IBB_HOST = 'https://i.ibb.co/';
 
 module.exports = {
+  logoStatic: `${IBB_HOST}PGJTHT5/bonefish-logo-static.png`,
   bioImage_Sharon: `${IBB_HOST}1dHH2kV/sharon-zaidins-bio-photo.jpg`,
   bioImage_Paul: `${IBB_HOST}dbZhcVB/paul-zaidins-bio-photo.jpg`,
   cfoOfTheYearLogo: `${IBB_HOST}kDY7CMJ/cfo-of-the-year-logo.jpg`,
